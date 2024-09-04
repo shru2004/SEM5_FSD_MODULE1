fe0beee33e8c4871f8a38ccb76e8249e1fc6d992
Shruthi S <sm.shruthi2004@gmail.com>
2024-09-04 08:20:41 +0530

    module 1

---
+++ javascript/spreadoperator.js
@@ -0,0 +1,16 @@
+const set1 = [10, 20, 30, 40, 50];
+const set2 = [60, 70, 80, 90, 100];
+console.log(set2);
+
+const MediaStreamAudioDestinationNode = {
+  sname: "Raj",
+  sage: 20
+};
+
+const sectionD = {
+  student: null, // or some other default value
+  no_student: 71
+};
+console.log(sectionD);
+const set3 = [...set1, ...set2];
+console.log(set3); // [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
